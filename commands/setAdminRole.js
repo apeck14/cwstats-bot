90d36199b0399d663a9054a5b212b513386aec89
apeck14 <aaronpeck_14@yahoo.com>
2021-08-02 13:15:12 -0500

    updated error messages

--- commands/setAdminRole.js
+++ commands/setAdminRole.js
@@ -14,7 +14,7 @@ module.exports = {
         const { commandChannelID } = channels;
         const guildOwnerID = message.guild.owner?.id;
 
-        if (guildOwnerID !== message.author.id && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send({ embed: { color: red, description: 'Only the **server owner** can set this role!' } });
+        if (guildOwnerID !== message.author.id && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send({ embed: { color: red, description: `Only the **server owner** or users with ADMINISTRATOR permissions can set this role!\n\n__Usage:__\n\`${prefix}setAdminRole @ROLE\`` } });
         else if (commandChannelID && commandChannelID !== message.channel.id) return message.channel.send({ embed: { color: red, description: `You can only use this command in the set **command channel**! (<#${commandChannelID}>)` } });
 
         let roleID = arg.substr(3); //remove first three characters "<@&"
