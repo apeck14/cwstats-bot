49e612f949ee6bf4a5b0de05ad8fe0f6249f2969
apeck14 <aaronpeck_14@yahoo.com>
2021-08-22 14:45:42 -0500

    removed update message

--- index.js
+++ index.js
@@ -37,13 +37,6 @@ bot.once('ready', async () => {
     console.log('CW2 Stats is online!');
 
     try {
-        bot.guilds.cache.each(async g => {
-            const guildInDb = await guilds.findOne({ guildID: g.id });
-
-            if (guildInDb && guildInDb.channels.commandChannelID)
-                bot.channels.cache.get(guildInDb.channels.commandChannelID).send(`**__Bot Update__**\n\nThe bot has recently reached it's maximum capacity of 100 Discord servers, and is now in the process of being verified vy Discord! :partying_face:\n\nHowever, clan badges and other emojis have been temporarily disabled, but all functionality will remain the same! Clan badges will be back very shortly.`);
-        });
-
         //make sure all current guilds have a spot in database, in case the bot joined while down
         bot.guilds.cache.each(async g => {
             const guildInDb = await guilds.findOne({ guildID: g.id });
