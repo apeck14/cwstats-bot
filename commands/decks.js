eb9ef324803419df8592cebfbfaaa9c5c145f5a9
Aaron Peck <aaronpeck_14@yahoo.com>
2021-09-07 02:46:41 -0500

    major efficiency improvement

--- commands/decks.js
+++ commands/decks.js
@@ -15,8 +15,6 @@ module.exports = {
         //must be in command channel if set
         if (commandChannelID && commandChannelID !== message.channel.id) return message.channel.send({ embed: { color: red, description: `You can only use this command in the set **command channel**! (<#${commandChannelID}>)` } });
 
-        return message.channel.send({ embed: { color: orange, description: `**Decks command temporarily disabled.** Will be back soon. :)` } });
-
         //account must be linked
         const linkedAccount = await linkedAccounts.findOne({ discordID: message.author.id });
         let tag;
@@ -27,90 +25,15 @@ module.exports = {
         const player = await getPlayerData(tag);
         if (!player) return message.channel.send({ embed: { color: red, description: `**Player not found.** Please re-link your tag with the correct tag.` } });
 
-        const cardsGroupedByLevel = groupBy(player.cards, c => 13 - (c.maxLevel - c.level));
+        const cardsGroupedByLevel = groupBy(player.cards.filter(c => c.maxLevel - c.level < 5), c => 13 - (c.maxLevel - c.level));
 
         for (const lvl in cardsGroupedByLevel) {
             cardsGroupedByLevel[lvl] = cardsGroupedByLevel[lvl].map(c => c.name.toLowerCase().replace(/\s+/g, '-').replace(/\./g, ''));
         }
 
-        const allDecks = await decks.find({}).toArray();
-
-        const deckSets = [];
-        let cardsAvailable = [];
-        let decksAvailable = [];
-        let lastLvlAdded;
-
-        function containsAllCards(deck) {
-            return deck.cards.every(c => cardsAvailable.includes(c));
+        function containsAllCards(d, cardsAvail) {
+            return d.cards.every(c => cardsAvail.includes(c));
         }
-
-        for (let lvl = 13; lvl >= 1; lvl--) {
-            if (cardsGroupedByLevel[`${lvl}`]) cardsAvailable = cardsAvailable.concat(cardsGroupedByLevel[`${lvl}`]);
-            lastLvlAdded = lvl;
-
-            if (cardsAvailable.length >= 32) break;
-        }
-
-        while (deckSets.length < 2) {
-            if (cardsAvailable.length > 80) decksAvailable = allDecks.filter(d => d.rating >= 51 && containsAllCards(d));
-            else if (cardsAvailable.length > 70) decksAvailable = allDecks.filter(d => d.rating >= 49 && containsAllCards(d));
-            else if (cardsAvailable.length > 50) decksAvailable = allDecks.filter(d => d.rating >= 45 && containsAllCards(d));
-            else decksAvailable = allDecks.filter(containsAllCards);
-
-            //get all possible deck sets
-            for (let i = 0, len = decksAvailable.length; i < len - 3; i++) {
-                let decksUsed = [decksAvailable[i]];
-
-                for (let x = i + 1; x < len; x++) {
-                    let allCardsAvailable = true;
-
-                    for (const c of decksAvailable[x].cards) {
-                        //check if card is in any of current decks
-                        for (const d of decksUsed) {
-                            if (d.cards.includes(c)) {
-                                allCardsAvailable = false;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (allCardsAvailable) decksUsed.push(decksAvailable[x]);
-                    if (decksUsed.length === 4) {
-                        deckSets.push(decksUsed);
-                        break;
-                    }
-                }
-            }
-
-            //add more cards to cardsAvailable
-            if (deckSets.length < 2) {
-                lastLvlAdded--;
-
-                if (cardsGroupedByLevel[`${lastLvlAdded}`]?.length > 0) cardsAvailable = cardsAvailable.concat(cardsGroupedByLevel[`${lastLvlAdded}`]);
-            }
-
-            if (lastLvlAdded <= 0) break;
-        }
-
-        if (deckSets.length === 0) return message.channel.send({ embed: { color: orange, description: `**No deck sets found.** More cards need to be unlocked.` } });
-        else if (deckSets.length === 2) {
-            if (deckSets[0][0]._id === deckSets[1][0]._id && deckSets[0][1]._id === deckSets[1][1]._id && deckSets[0][2]._id === deckSets[1][2]._id && deckSets[0][3]._id === deckSets[1][3]._id) {
-                deckSets.pop();
-            }
-        }
-
-        deckSets.sort((a, b) => {
-            const avgRatingA = average(a.map(d => d.rating));
-            const avgRatingB = average(b.map(d => d.rating));
-            const avgLvlA = getAvgLvl(a);
-            const avgLvlB = getAvgLvl(b);
-
-            if (avgRatingA === avgRatingB) {
-                if (avgLvlA === avgLvlB) return average(a.map(d => new Date(d.dateAdded).getTime())) - average(b.map(d => new Date(d.dateAdded).getTime()));
-                return avgLvlB - avgLvlA;
-            }
-            return avgRatingB - avgRatingA;
-        });
 
         function getDeckUrl(cards) {
             const cardData = [
@@ -228,6 +151,16 @@ module.exports = {
             return url.substring(0, url.length - 1);
         }
 
+        function shareCards(deck1, deck2, deck3, deck4) {
+            let cards;
+
+            if (deck4) cards = deck1.cards.concat(deck2.cards, deck3.cards, deck4.cards);
+            else if (deck3) cards = deck1.cards.concat(deck2.cards, deck3.cards);
+            else cards = deck1.cards.concat(deck2.cards);
+
+            return cards.length !== new Set(cards).size;
+        }
+
         function getAvgLvl(deckSet) {
             let sum = 0;
 
@@ -240,6 +173,105 @@ module.exports = {
 
             return sum / 32;
         }
+
+        function totalRating(deckSet) {
+            let sum = 0;
+
+            for (const d of deckSet) {
+                sum += d.rating;
+            }
+
+            return sum;
+        }
+
+        let cardsAvailable = [];
+        let lastLvlAdded;
+
+        for (let lvl = 13; lvl >= 1; lvl--) {
+            if (cardsGroupedByLevel[`${lvl}`]) cardsAvailable = cardsAvailable.concat(cardsGroupedByLevel[`${lvl}`]);
+            lastLvlAdded = lvl;
+
+            if (cardsAvailable.length >= 32) break;
+        }
+
+        if (cardsAvailable.length < 32) return message.channel.send({ embed: { color: orange, description: `**No deck sets found.** More cards need to be unlocked.` } });
+
+        const allDecks = (await decks.find({}).toArray()).sort((a, b) => b.rating - a.rating);
+        const deckSets = [];
+
+        while (deckSets.length < 2) {
+            //get all possible deck sets
+            for (let i = 0, len = allDecks.length; i < len - 3; i++) {
+                let deck1 = allDecks[i];
+                if (deckSets.length === 2 && deck1.rating * 4 <= totalRating(deckSets[1])) break;
+                else if (!containsAllCards(deck1, cardsAvailable)) continue;
+
+                for (let x = i + 1; x < len - 2; x++) {
+                    let deck2 = allDecks[x];
+                    if (deckSets.length === 2 && deck1.rating + (deck2.rating * 3) <= totalRating(deckSets[1])) continue;
+                    else if (!containsAllCards(deck2, cardsAvailable)) continue;
+                    else if (shareCards(deck1, deck2)) continue;
+
+                    for (let y = x + 1; y < len - 1; y++) {
+                        let deck3 = allDecks[y];
+                        if (deckSets.length === 2 && (deck1.rating + deck2.rating) + (deck3.rating * 2) <= totalRating(deckSets[1])) continue;
+                        else if (!containsAllCards(deck3, cardsAvailable)) continue;
+                        else if (shareCards(deck1, deck2, deck3)) continue;
+
+                        for (let z = y + 1; z < len; z++) {
+                            let deck4 = allDecks[z];
+                            if (deckSets.length === 2 && (deck1.rating + deck2.rating + deck3.rating + deck4.rating) <= totalRating(deckSets[1])) continue;
+                            else if (!containsAllCards(deck4, cardsAvailable)) continue;
+                            else if (shareCards(deck1, deck2, deck3, deck4)) continue;
+
+                            //add to deckSets if higher rated
+                            if (deckSets.length === 0) deckSets.push([deck1, deck2, deck3, deck4]);
+                            else if (deckSets.length === 1) {
+                                const isHigherThan = totalRating([deck1, deck2, deck3, deck4]) > totalRating(deckSets[0]);
+                                if (isHigherThan) deckSets.unshift([deck1, deck2, deck3, deck4]); //add to front
+                                else deckSets.push([deck1, deck2, deck3, deck4]); //add to end
+                            }
+                            else if (deckSets.length === 2) {
+                                const [deckSet1Rat, deckSet2Rat, deckSetRat] = [totalRating(deckSets[0]), totalRating(deckSets[1]), totalRating([deck1, deck2, deck3, deck4])];
+
+                                if (deckSetRat > deckSet1Rat) {
+                                    deckSets.pop();
+                                    deckSets.unshift([deck1, deck2, deck3, deck4]);
+                                }
+                                else if (deckSetRat > deckSet2Rat) {
+                                    deckSets.pop();
+                                    deckSets.push([deck1, deck2, deck3, deck4]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //add more cards to cardsAvailable
+            if (deckSets.length < 2) {
+                lastLvlAdded--;
+
+                if (cardsGroupedByLevel[`${lastLvlAdded}`]?.length > 0) cardsAvailable = cardsAvailable.concat(cardsGroupedByLevel[`${lastLvlAdded}`]);
+            }
+
+            if (lastLvlAdded <= 0) break;
+        }
+
+        if (deckSets.length === 0) return message.channel.send({ embed: { color: orange, description: `**No deck sets found.** More cards need to be unlocked.` } });
+
+        deckSets.sort((a, b) => {
+            const avgRatingA = average(a.map(d => d.rating));
+            const avgRatingB = average(b.map(d => d.rating));
+            const avgLvlA = getAvgLvl(a);
+            const avgLvlB = getAvgLvl(b);
+
+            if (avgRatingA === avgRatingB) {
+                if (avgLvlA === avgLvlB) return average(a.map(d => new Date(d.dateAdded).getTime())) - average(b.map(d => new Date(d.dateAdded).getTime()));
+                return avgLvlB - avgLvlA;
+            }
+            return avgRatingB - avgRatingA;
+        });
 
         let desc = `\n**__Best War Deck Set__**\nRating: **${(average(deckSets[0].map(d => d.rating))).toFixed(1)}**\nAvg. Lvl: **${getAvgLvl(deckSets[0]).toFixed(1)}**\n`;
 
