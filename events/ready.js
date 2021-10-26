14de609803316bd49a3a1c908d75fdca7990b3a8
Aaron Peck <aaronpeck_14@yahoo.com>
2021-10-25 20:20:06 -0500

    send update message

--- events/ready.js
+++ events/ready.js
@@ -12,22 +12,25 @@ module.exports = {
                 const guildInDb = await guilds.findOne({ guildID: g.id });
 
                 if (guildInDb && guildInDb.channels.commandChannelID) {
-                    const channelPermissions = bot.channels.cache.get(guildInDb.channels.commandChannelID).permissionsFor(bot.user);
-                    const requiredPerms = ['SEND_MESSAGES', 'EMBED_LINKS', 'VIEW_CHANNEL'];
-                    const missingPerms = requiredPerms.filter(p => !channelPermissions.has(p));
+                    const channel = bot.channels.cache.get(guildInDb.channels.commandChannelID);
+                    if (channel) {
+                        const channelPermissions = channel.permissionsFor(bot.user);
+                        const requiredPerms = ['SEND_MESSAGES', 'EMBED_LINKS', 'VIEW_CHANNEL'];
+                        const missingPerms = requiredPerms.filter(p => !channelPermissions.has(p));
 
-                    //check permissions
-                    if (missingPerms.length === 0) {
-                        bot.channels.cache.get(guildInDb.channels.commandChannelID).send({
-                            embed: {
-                                title: '__New Update!__ (10/25/2021)',
-                                thumbnail: {
-                                    url: 'https://i.postimg.cc/Nj2CWzQc/CW2-Stats.png'
-                                },
-                                color: '#ff237a',
-                                description: `**Clan families, new command, quality of life improvements...and more!**\n\nTo view the full details, use the **${guildInDb.prefix}update** command!`
-                            }
-                        });
+                        //check permissions
+                        if (missingPerms.length === 0) {
+                            bot.channels.cache.get(guildInDb.channels.commandChannelID).send({
+                                embed: {
+                                    title: '__New Update!__ (10/25/2021)',
+                                    thumbnail: {
+                                        url: 'https://i.postimg.cc/Nj2CWzQc/CW2-Stats.png'
+                                    },
+                                    color: '#ff237a',
+                                    description: `**Clan families, new command, quality of life improvements...and more!**\n\nTo view the full details, use the **${guildInDb.prefix}update** command!`
+                                }
+                            });
+                        }
                     }
                 }
             });
