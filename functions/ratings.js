d3d86cc01fd6f34a82479f014ac5b4be9440cfd4
Aaron Peck <aaronpeck_14@yahoo.com>
2021-10-29 16:21:46 -0500

    tweaked ratings

--- functions/ratings.js
+++ functions/ratings.js
@@ -1,74 +1,62 @@
 module.exports = {
-    /*
-        100: 7500
-        80: 6900
-        60: 6300
-        40: 5700
-        20: 5100
-        0: 4500
-    */
     pbRating: (player) => {
         if (!player.pb) return 0;
 
-        if (player.pb >= 7500) return 100;
-        else if (player.pb <= 4500) return 0;
+        const max = 8000; // 100 rating
+        const min = 3000; // 0 rating
 
-        return ((player.pb - 4500) / 3000) * 100;
+        if (player.pb >= max) return 100;
+        else if (player.pb <= min) return 0;
+
+        return ((player.pb - min) / (max - min)) * 100;
     },
-    /*
-        Lvl 13 = +1
-        Lvl 12 = +0.5
-        Lvl 11 = +0.2
-        Lvl 10 = +0.08
-        Lvl 9 = +0.04
-
-        100: 100+
-        80: 80
-        60: 60
-        40: 40
-        20: 20
-        0: 0
-    */
     cardsRating: (player) => {
         if (!player.cards) return 0;
 
-        let sum = 0;
+        let rating = 0;
 
         for (const c of player.cards) {
             const diff = c.maxLevel - c.level;
 
-            if (diff === 0) sum++; //level 14
-            else if (diff === 1) sum += 0.75; //level 13
-            else if (diff === 2) sum += 0.35; //level 12
-            else if (diff === 3) sum += 0.15; //level 11
-            else if (diff === 4) sum += 0.08; //level 10
-            else if (diff === 5) sum += 0.04; //level 9
+            if (diff === 0) rating++; //level 14
+            else if (diff === 1) rating += 0.75; //level 13
+            else if (diff === 2) rating += 0.35; //level 12
+            else if (diff === 3) rating += 0.15; //level 11
+            else if (diff === 4) rating += 0.08; //level 10
+            else if (diff === 5) rating += 0.04; //level 9
+            else if (diff === 5) rating += 0.02; //level 8
+            else if (diff === 5) rating += 0.01; //level 7
+            else if (diff === 5) rating += 0.004; //level 6
+            else if (diff === 5) rating += 0.0015; //level 5
+            else if (diff === 5) rating += 0.0005; //level 4
+            else if (diff === 5) rating += 0.0002; //level 3
+            else if (diff === 5) rating += 0.00005; //level 2
         }
 
-        return sum;
+        return rating;
     },
-    /*
-        Challenges: (30%)
-        GC = +50
-        CC = +5
-
-        100: 100+
-        80: 80
-        60: 60
-        40: 40
-        20: 20
-        0: 0
-
-        Most Chall Wins: (70%)
-        100: 20
-        80: 16
-        60: 12
-        40: 8
-        20: 4
-        0: 0
-    */
     challRating: (player) => {
-        if (!player.challWins) return 0;
+        /*
+            Challenges: (30%)
+            GC = +50
+            CC = +5
+
+            100: 100+
+            80: 80
+            60: 60
+            40: 40
+            20: 20
+            0: 0
+
+            Most Chall Wins: (70%)
+            100: 20
+            80: 16
+            60: 12
+            40: 8
+            20: 4
+            0: 0
+        */
+        if (isNaN(player.challWins)) return 0;
 
         let challengesRating = (player.challWins * 5) + (player.grandChallWins * 50);
         const mostChallWinsRating = player.mostChallWins * 5;
@@ -77,18 +65,12 @@ module.exports = {
 
         return (challengesRating * 0.3) + (mostChallWinsRating * 0.7);
     },
-    /*
-        100: 325+
-        80: 260
-        60: 195
-        40: 130
-        20: 65
-        0: 0
-    */
     cw1Rating: (player) => {
         if (!player.warWins) return 0;
 
-        if (player.warWins >= 350) return 100;
+        const max = 350; // 100 rating
+
+        if (player.warWins >= max) return 100;
         return player.warWins / 3.5;
     }
 }
