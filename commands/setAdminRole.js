002abe14dc7c419f08a44e7ceb19e7613c5fc7d8
Aaron Peck <aaronpeck_14@yahoo.com>
2021-11-14 18:26:30 -0600

    Discord v13 Update

--- commands/setAdminRole.js
+++ commands/setAdminRole.js
@@ -16,7 +16,7 @@ module.exports = {
         const { commandChannelID } = channels;
         const guildOwnerID = message.guild.owner?.id;
 
-        if ((guildOwnerID !== message.author.id && !message.member.hasPermission('ADMINISTRATOR')) && message.author.id !== '493245767448789023') throw `Only the **server owner** or users with ADMINISTRATOR permissions can set this role!\n\n__Usage:__\n\`${prefix}setAdminRole @ROLE\``
+        if ((guildOwnerID !== message.author.id && !message.member.permissions.has('ADMINISTRATOR')) && message.author.id !== '493245767448789023') throw `Only the **server owner** or users with ADMINISTRATOR permissions can set this role!\n\n__Usage:__\n\`${prefix}setAdminRole @ROLE\``
         else if (commandChannelID && commandChannelID !== message.channel.id) throw `You can only use this command in the set **command channel**! (<#${commandChannelID}>)`;
 
         if (!args[0]) throw `**No role given!** Try again.\n\n__Usage:__\n\`${prefix}setAdminRole @ROLE\``;
