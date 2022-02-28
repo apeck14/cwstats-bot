e4575ea82ef8a0197720aa355a126e232b462da3
Aaron Peck <aaronpeck_14@yahoo.com>
2022-02-27 19:47:37 -0600

    add new guilds when bot is down

--- src/events/ready.js
+++ src/events/ready.js
@@ -20,5 +20,26 @@ module.exports = {
         const finalArray = commandsArray.map((e) => e.data);
         slash.register(client.user.id, finalArray);
         console.log(`${client.user.tag} Started`);
+
+        //make sure all current guilds have a spot in database, in case the bot joined while down
+        const guilds = db.collection('Guilds');
+
+        client.guilds.cache.each(async g => {
+            const guildInDb = await guilds.findOne({ guildID: g.id });
+
+            if (!guildInDb) {
+                guilds.insertOne({
+                    guildID: guild.id,
+                    channels: {
+                        applyChannelID: null,
+                        applicationsChannelID: null,
+                        commandChannelID: null
+                    },
+                    abbreviations: []
+                });
+
+                console.log(`JOINED GUILD: ${guild.name} (${guild.id})`);
+            }
+        });
     },
 };
