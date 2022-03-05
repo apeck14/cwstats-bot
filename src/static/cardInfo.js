a2251845c7d030ca520b7a79da044611cba41930
Aaron Peck <aaronpeck_14@yahoo.com>
2022-03-04 23:27:32 -0600

    added card rarities

--- src/static/cardInfo.js
+++ src/static/cardInfo.js
@@ -1,240 +1,427 @@
 module.exports = [
-    { name: 'archer-queen', aliases: ['queen', 'aq'], id: 26000072 },
-    { name: 'archers', aliases: [], id: 26000001 },
-    { name: 'arrows', aliases: [], id: 28000001 },
+    {
+        name: 'archer-queen',
+        aliases: ['queen', 'aq'],
+        id: 26000072,
+        rarity: 'champion'
+    },
+    { name: 'archers', aliases: [], id: 26000001, rarity: 'common' },
+    { name: 'arrows', aliases: [], id: 28000001, rarity: 'common' },
     {
         name: 'baby-dragon',
         aliases: ['bdrag', 'baby-d', 'bd'],
-        id: 26000015
+        id: 26000015,
+        rarity: 'epic'
     },
-    { name: 'balloon', aliases: ['loon'], id: 26000006 },
-    { name: 'bandit', aliases: [], id: 26000046 },
+    {
+        name: 'balloon',
+        aliases: ['loon'],
+        id: 26000006,
+        rarity: 'epic'
+    },
+    { name: 'bandit', aliases: [], id: 26000046, rarity: 'legendary' },
     {
         name: 'barbarian-barrel',
         aliases: ['barb-barrel', 'bb'],
-        id: 28000015
+        id: 28000015,
+        rarity: 'epic'
     },
-    { name: 'barbarian-hut', aliases: ['barb-hut'], id: 27000005 },
-    { name: 'barbarians', aliases: ['barbs'], id: 26000008 },
-    { name: 'bats', aliases: [], id: 26000049 },
-    { name: 'battle-healer', aliases: ['healer'], id: 26000068 },
-    { name: 'battle-ram', aliases: ['br'], id: 26000036 },
-    { name: 'bomb-tower', aliases: ['bt'], id: 27000004 },
-    { name: 'bomber', aliases: [], id: 26000013 },
-    { name: 'bowler', aliases: [], id: 26000034 },
-    { name: 'cannon', aliases: [], id: 27000000 },
-    { name: 'cannon-cart', aliases: ['cart', 'cc'], id: 26000054 },
-    { name: 'clone', aliases: [], canBeUnderLeveled: true, id: 28000013 },
-    { name: 'dark-prince', aliases: ['dp'], id: 26000027 },
-    { name: 'dart-goblin', aliases: ['dart-gob', 'dg'], id: 26000040 },
-    { name: 'earthquake', aliases: ['quake', 'eq'], id: 28000014 },
+    {
+        name: 'barbarian-hut',
+        aliases: ['barb-hut'],
+        id: 27000005,
+        rarity: 'rare'
+    },
+    {
+        name: 'barbarians',
+        aliases: ['barbs'],
+        id: 26000008,
+        rarity: 'common'
+    },
+    { name: 'bats', aliases: [], id: 26000049, rarity: 'common' },
+    {
+        name: 'battle-healer',
+        aliases: ['healer'],
+        id: 26000068,
+        rarity: 'rare'
+    },
+    {
+        name: 'battle-ram',
+        aliases: ['br'],
+        id: 26000036,
+        rarity: 'rare'
+    },
+    {
+        name: 'bomb-tower',
+        aliases: ['bt'],
+        id: 27000004,
+        rarity: 'rare'
+    },
+    { name: 'bomber', aliases: [], id: 26000013, rarity: 'common' },
+    { name: 'bowler', aliases: [], id: 26000034, rarity: 'epic' },
+    { name: 'cannon', aliases: [], id: 27000000, rarity: 'common' },
+    {
+        name: 'cannon-cart',
+        aliases: ['cart', 'cc'],
+        id: 26000054,
+        rarity: 'epic'
+    },
+    {
+        name: 'clone',
+        aliases: [],
+        canBeUnderLeveled: true,
+        id: 28000013,
+        rarity: 'epic'
+    },
+    {
+        name: 'dark-prince',
+        aliases: ['dp'],
+        id: 26000027,
+        rarity: 'epic'
+    },
+    {
+        name: 'dart-goblin',
+        aliases: ['dart-gob', 'dg'],
+        id: 26000040,
+        rarity: 'rare'
+    },
+    {
+        name: 'earthquake',
+        aliases: ['quake', 'eq'],
+        id: 28000014,
+        rarity: 'rare'
+    },
     {
         name: 'electro-dragon',
         aliases: ['edrag', 'electro-drag', 'e-drag', 'ed'],
-        id: 26000063
+        id: 26000063,
+        rarity: 'epic'
     },
     {
         name: 'electro-giant',
         aliases: ['egiant', 'e-giant'],
-        id: 26000085
+        id: 26000085,
+        rarity: 'epic'
     },
     {
         name: 'electro-spirit',
         aliases: ['espirit', 'e-spirit', 'es'],
         canBeUnderLeveled: true,
-        id: 26000084
+        id: 26000084,
+        rarity: 'common'
     },
     {
         name: 'electro-wizard',
         aliases: ['ewiz', 'e-wiz', 'ew'],
-        id: 26000042
+        id: 26000042,
+        rarity: 'legendary'
     },
     {
         name: 'elite-barbarians',
         aliases: ['ebarbs', 'e-barbs', 'eb'],
-        id: 26000043
+        id: 26000043,
+        rarity: 'common'
     },
     {
         name: 'elixir-collector',
         aliases: ['collector', 'ecollector', 'e-collector', 'ec'],
-        id: 27000007
+        id: 27000007,
+        rarity: 'rare'
     },
     {
         name: 'elixir-golem',
         aliases: ['egolem', 'e-golem'],
-        id: 26000067
+        id: 26000067,
+        rarity: 'rare'
     },
-    { name: 'executioner', aliases: ['exe', 'exec'], id: 26000045 },
+    {
+        name: 'executioner',
+        aliases: ['exe', 'exec'],
+        id: 26000045,
+        rarity: 'epic'
+    },
     {
         name: 'fire-spirit',
         aliases: ['fspirit', 'f-spirit', 'fs'],
-        id: 26000031
+        id: 26000031,
+        rarity: 'common'
     },
-    { name: 'fireball', aliases: ['fball', 'fb'], id: 28000000 },
-    { name: 'firecracker', aliases: ['fc'], id: 26000064 },
+    {
+        name: 'fireball',
+        aliases: ['fball', 'fb'],
+        id: 28000000,
+        rarity: 'rare'
+    },
+    {
+        name: 'firecracker',
+        aliases: ['fc'],
+        id: 26000064,
+        rarity: 'common'
+    },
     {
         name: 'fisherman',
         aliases: [],
         canBeUnderLeveled: true,
-        id: 26000061
+        id: 26000061,
+        rarity: 'legendary'
     },
     {
         name: 'flying-machine',
         aliases: ['fmachine', 'f-machine', 'fm'],
-        id: 26000057
+        id: 26000057,
+        rarity: 'rare'
     },
     {
         name: 'freeze',
         aliases: [],
         canBeUnderLeveled: true,
-        id: 28000005
+        id: 28000005,
+        rarity: 'epic'
     },
-    { name: 'furnace', aliases: [], id: 27000010 },
-    { name: 'giant', aliases: [], id: 26000003 },
+    { name: 'furnace', aliases: [], id: 27000010, rarity: 'rare' },
+    { name: 'giant', aliases: [], id: 26000003, rarity: 'rare' },
     {
         name: 'giant-skeleton',
         aliases: ['giant-skelly', 'gs'],
-        id: 26000020
+        id: 26000020,
+        rarity: 'epic'
     },
     {
         name: 'giant-snowball',
         aliases: ['snowball'],
         canBeUnderLeveled: true,
-        id: 28000017
+        id: 28000017,
+        rarity: 'common'
     },
     {
         name: 'goblin-barrel',
         aliases: ['gobbarrel', 'gob-barrel', 'gb'],
-        id: 28000004
+        id: 28000004,
+        rarity: 'epic'
     },
     {
         name: 'goblin-cage',
         aliases: ['cage', 'gobcage', 'gob-cage', 'gc'],
-        id: 27000012
+        id: 27000012,
+        rarity: 'rare'
     },
     {
         name: 'goblin-drill',
         aliases: ['drill', 'gobdrill', 'gob-drill', 'gd'],
-        id: 27000013
+        id: 27000013,
+        rarity: 'epic'
     },
     {
         name: 'goblin-gang',
         aliases: ['gang', 'gobgang', 'gob-gang'],
-        id: 26000041
+        id: 26000041,
+        rarity: 'common'
     },
     {
         name: 'goblin-giant',
         aliases: ['gobgiant', 'gob-giant', 'ggiant', 'g-giant'],
-        id: 26000060
+        id: 26000060,
+        rarity: 'epic'
     },
     {
         name: 'goblin-hut',
         aliases: ['gobhut', 'gob-hut', 'gh'],
-        id: 27000001
+        id: 27000001,
+        rarity: 'rare'
     },
-    { name: 'goblins', aliases: ['gobs'], id: 26000002 },
+    {
+        name: 'goblins',
+        aliases: ['gobs'],
+        id: 26000002,
+        rarity: 'common'
+    },
     {
         name: 'golden-knight',
         aliases: ['g-knight', 'gknight', 'gk'],
-        id: 26000074
+        id: 26000074,
+        rarity: 'champion'
     },
-    { name: 'golem', aliases: [], id: 26000009 },
-    { name: 'graveyard', aliases: ['gy'], id: 28000010 },
-    { name: 'guards', aliases: [], id: 26000025 },
+    { name: 'golem', aliases: [], id: 26000009, rarity: 'epic' },
+    {
+        name: 'graveyard',
+        aliases: ['gy'],
+        id: 28000010,
+        rarity: 'legendary'
+    },
+    { name: 'guards', aliases: [], id: 26000025, rarity: 'epic' },
     {
         name: 'heal-spirit',
         aliases: ['h-spirit', 'hspirit', 'hs'],
         canBeUnderLeveled: true,
-        id: 28000016
+        id: 28000016,
+        rarity: 'rare'
     },
-    { name: 'hog-rider', aliases: ['hog', 'hr'], id: 26000021 },
-    { name: 'hunter', aliases: [], id: 26000044 },
+    {
+        name: 'hog-rider',
+        aliases: ['hog', 'hr'],
+        id: 26000021,
+        rarity: 'rare'
+    },
+    { name: 'hunter', aliases: [], id: 26000044, rarity: 'epic' },
     {
         name: 'ice-golem',
         aliases: ['i-golem', 'igolem', 'ig'],
-        id: 26000038
+        id: 26000038,
+        rarity: 'rare'
     },
     {
         name: 'ice-spirit',
         aliases: ['i-spirit', 'ispirit', 'is'],
         canBeUnderLeveled: true,
-        id: 26000030
+        id: 26000030,
+        rarity: 'common'
     },
     {
         name: 'ice-wizard',
         aliases: ['icewiz', 'ice-wiz', 'iwiz', 'i-wiz', 'iw'],
-        id: 26000023
+        id: 26000023,
+        rarity: 'legendary'
     },
     {
         name: 'inferno-dragon',
         aliases: ['idrag', 'i-drag', 'infernodrag', 'inferno-drag', 'id'],
         canBeUnderLeveled: true,
-        id: 26000037
+        id: 26000037,
+        rarity: 'legendary'
     },
     {
         name: 'inferno-tower',
         aliases: ['itower', 'i-tower', 'it'],
         canBeUnderLeveled: true,
-        id: 27000003
+        id: 27000003,
+        rarity: 'rare'
     },
-    { name: 'knight', aliases: ['knight', 'knight'], id: 26000000 },
+    {
+        name: 'knight',
+        aliases: ['knight', 'knight'],
+        id: 26000000,
+        rarity: 'common'
+    },
     {
         name: 'lava-hound',
         aliases: ['hound', 'lava', 'lh'],
-        id: 26000029
+        id: 26000029,
+        rarity: 'legendary'
     },
-    { name: 'lightning', aliases: [], id: 28000007 },
-    { name: 'lumberjack', aliases: ['ljack', 'lj'], id: 26000035 },
+    { name: 'lightning', aliases: [], id: 28000007, rarity: 'epic' },
+    {
+        name: 'lumberjack',
+        aliases: ['ljack', 'lj'],
+        id: 26000035,
+        rarity: 'legendary'
+    },
     {
         name: 'magic-archer',
         aliases: ['march', 'marcher', 'ma'],
-        id: 26000062
+        id: 26000062,
+        rarity: 'legendary'
     },
-    { name: 'mega-knight', aliases: ['mk'], id: 26000055 },
-    { name: 'mega-minion', aliases: ['mm'], id: 26000039 },
-    { name: 'miner', aliases: [], id: 26000032 },
+    {
+        name: 'mega-knight',
+        aliases: ['mk'],
+        id: 26000055,
+        rarity: 'legendary'
+    },
+    {
+        name: 'mega-minion',
+        aliases: ['mm'],
+        id: 26000039,
+        rarity: 'rare'
+    },
+    { name: 'miner', aliases: [], id: 26000032, rarity: 'legendary' },
     {
         name: 'mini-pekka',
         aliases: ['minipekka', 'mini-pekka', 'mp'],
-        id: 26000018
+        id: 26000018,
+        rarity: 'rare'
     },
-    { name: 'minion-horde', aliases: ['mh'], id: 26000022 },
-    { name: 'minions', aliases: [], id: 26000005 },
-    { name: 'mirror', aliases: [], id: 28000006 },
-    { name: 'mortar', aliases: [], id: 27000002 },
-    { name: 'mother-witch', aliases: ['mw'], id: 26000083 },
-    { name: 'musketeer', aliases: ['musk'], id: 26000014 },
-    { name: 'night-witch', aliases: ['nw'], id: 26000048 },
-    { name: 'pekka', aliases: ['pekka'], id: 26000004 },
-    { name: 'poison', aliases: [], id: 28000009 },
-    { name: 'prince', aliases: [], id: 26000016 },
-    { name: 'princess', aliases: [], id: 26000026 },
-    { name: 'rage', aliases: [], canBeUnderLeveled: true, id: 28000002 },
-    { name: 'ram-rider', aliases: [], id: 26000051 },
-    { name: 'rascals', aliases: [], id: 26000053 },
-    { name: 'rocket', aliases: [], id: 28000003 },
+    {
+        name: 'minion-horde',
+        aliases: ['mh'],
+        id: 26000022,
+        rarity: 'common'
+    },
+    { name: 'minions', aliases: [], id: 26000005, rarity: 'common' },
+    { name: 'mirror', aliases: [], id: 28000006, rarity: 'epic' },
+    { name: 'mortar', aliases: [], id: 27000002, rarity: 'common' },
+    {
+        name: 'mother-witch',
+        aliases: ['mw'],
+        id: 26000083,
+        rarity: 'legendary'
+    },
+    {
+        name: 'musketeer',
+        aliases: ['musk'],
+        id: 26000014,
+        rarity: 'rare'
+    },
+    {
+        name: 'night-witch',
+        aliases: ['nw'],
+        id: 26000048,
+        rarity: 'legendary'
+    },
+    { name: 'pekka', aliases: ['pekka'], id: 26000004, rarity: 'epic' },
+    { name: 'poison', aliases: [], id: 28000009, rarity: 'epic' },
+    { name: 'prince', aliases: [], id: 26000016, rarity: 'epic' },
+    { name: 'princess', aliases: [], id: 26000026, rarity: 'legendary' },
+    {
+        name: 'rage',
+        aliases: [],
+        canBeUnderLeveled: true,
+        id: 28000002,
+        rarity: 'epic'
+    },
+    { name: 'ram-rider', aliases: [], id: 26000051, rarity: 'legendary' },
+    { name: 'rascals', aliases: [], id: 26000053, rarity: 'common' },
+    { name: 'rocket', aliases: [], id: 28000003, rarity: 'rare' },
     {
         name: 'royal-delivery',
         aliases: ['delivery', 'rd'],
-        id: 28000018
+        id: 28000018,
+        rarity: 'common'
     },
-    { name: 'royal-ghost', aliases: ['ghost'], id: 26000050 },
-    { name: 'royal-giant', aliases: ['rg'], id: 26000024 },
+    {
+        name: 'royal-ghost',
+        aliases: ['ghost'],
+        id: 26000050,
+        rarity: 'legendary'
+    },
+    {
+        name: 'royal-giant',
+        aliases: ['rg'],
+        id: 26000024,
+        rarity: 'common'
+    },
     {
         name: 'royal-hogs',
         aliases: ['pigs', 'hogs', 'rh'],
-        id: 26000059
+        id: 26000059,
+        rarity: 'rare'
     },
     {
         name: 'royal-recruits',
         aliases: ['recruits', 'rr'],
-        id: 26000047
+        id: 26000047,
+        rarity: 'common'
     },
-    { name: 'skeleton-army', aliases: ['skarmy', 'sa'], id: 26000012 },
+    {
+        name: 'skeleton-army',
+        aliases: ['skarmy', 'sa'],
+        id: 26000012,
+        rarity: 'epic'
+    },
     {
         name: 'skeleton-barrel',
         aliases: ['skellybarrel', 'skelly-barrel', 'sbarrel', 's-barrel', 'sb'],
-        id: 26000056
+        id: 26000056,
+        rarity: 'common'
     },
     {
         name: 'skeleton-dragons',
@@ -245,44 +432,71 @@ module.exports = [
             'skelly-drags',
             'sd'
         ],
-        id: 26000080
+        id: 26000080,
+        rarity: 'common'
     },
     {
         name: 'skeleton-king',
         aliases: ['king', 'skellyking', 'skelly-king', 'sk'],
-        id: 26000069
+        id: 26000069,
+        rarity: 'champion'
     },
     {
         name: 'skeletons',
         aliases: ['skellies'],
         canBeUnderLeveled: true,
-        id: 26000010
+        id: 26000010,
+        rarity: 'common'
     },
-    { name: 'sparky', aliases: [], id: 26000033 },
+    { name: 'sparky', aliases: [], id: 26000033, rarity: 'legendary' },
     {
         name: 'spear-goblins',
         aliases: ['speargobs', 'spear-gobs', 'sgobs', 's-gobs', 'sg'],
-        id: 26000019
+        id: 26000019,
+        rarity: 'common'
     },
-    { name: 'tesla', aliases: [], id: 27000006 },
-    { name: 'the-log', aliases: ['log'], id: 28000011 },
+    { name: 'tesla', aliases: [], id: 27000006, rarity: 'common' },
+    {
+        name: 'the-log',
+        aliases: ['log'],
+        id: 28000011,
+        rarity: 'legendary'
+    },
     {
         name: 'three-musketeers',
         aliases: ['threemusks', 'three-musks', '3m', 'tm'],
-        id: 26000028
+        id: 26000028,
+        rarity: 'rare'
     },
-    { name: 'tombstone', aliases: ['ts'], canBeUnderLeveled: true, id: 27000009 },
+    {
+        name: 'tombstone',
+        aliases: ['ts'],
+        canBeUnderLeveled: true,
+        id: 27000009,
+        rarity: 'rare'
+    },
     {
         name: 'tornado',
         aliases: ['nado'],
         canBeUnderLeveled: true,
-        id: 28000012
+        id: 28000012,
+        rarity: 'epic'
     },
-    { name: 'valkyrie', aliases: ['valk'], id: 26000011 },
-    { name: 'wall-breakers', aliases: ['wb'], id: 26000058 },
-    { name: 'witch', aliases: [], id: 26000007 },
-    { name: 'wizard', aliases: ['wiz'], id: 26000017 },
-    { name: 'x-bow', aliases: ['xbow'], id: 27000008 },
-    { name: 'zap', aliases: [], id: 28000008 },
-    { name: 'zappies', aliases: [], id: 26000052 }
+    {
+        name: 'valkyrie',
+        aliases: ['valk'],
+        id: 26000011,
+        rarity: 'rare'
+    },
+    {
+        name: 'wall-breakers',
+        aliases: ['wb'],
+        id: 26000058,
+        rarity: 'epic'
+    },
+    { name: 'witch', aliases: [], id: 26000007, rarity: 'epic' },
+    { name: 'wizard', aliases: ['wiz'], id: 26000017, rarity: 'rare' },
+    { name: 'x-bow', aliases: ['xbow'], id: 27000008, rarity: 'epic' },
+    { name: 'zap', aliases: [], id: 28000008, rarity: 'common' },
+    { name: 'zappies', aliases: [], id: 26000052, rarity: 'rare' }
 ]
