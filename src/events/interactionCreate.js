9ea74e8f3fd3c36f65b30d27a5bec44a9e3b8bb6
Aaron Peck <aaronpeck_14@yahoo.com>
2022-03-08 22:19:55 -0600

    bug fix

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -4,7 +4,7 @@ module.exports = {
     event: "interactionCreate",
     run: async (client, db, i) => {
         if (!i.isCommand()) return;
-        if (!i.guild) return i.editReply({
+        if (!i.guild) return i.reply({
             embeds: [{
                 description: `**[Invite](https://discord.com/api/oauth2/authorize?client_id=869761158763143218&permissions=280576&scope=bot%20applications.commands) me to a server to use my commands!**`,
                 color: orange
