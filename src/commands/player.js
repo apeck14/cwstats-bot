3622b50666d28e5aca25f630b17b090e540574d4
Aaron Peck <aaronpeck_14@yahoo.com>
2022-04-06 05:01:53 -0500

    added arena + rank? to thumbnail

--- src/commands/player.js
+++ src/commands/player.js
@@ -1,182 +1,139 @@
-const { getPlayer, getClan } = require("../util/api");
-const { orange, pink } = require('../static/colors');
-const { getClanBadge, getEmoji, getArenaEmoji, formatTag, getLeague, sortArrOfBadges } = require("../util/functions");
-const { createCanvas, registerFont, loadImage } = require("canvas");
+const { getPlayer, getClan, getPlayerRanking } = require("../util/api")
+const { orange, pink } = require("../static/colors")
+const { getClanBadge, getEmoji, getArenaEmoji, formatTag } = require("../util/functions")
+const { createCanvas, registerFont, loadImage } = require("canvas")
+registerFont("./src/static/fonts/Supercell-Magic.ttf", { family: "Supercell-Magic" })
 
 module.exports = {
-    data: {
-        name: 'player',
-        description: 'View player stats.',
-        options: [
-            {
-                type: 3,
-                name: 'tag',
-                description: '#PLAYERTAG',
-                required: false
-            },
-            {
-                type: 6,
-                name: 'user',
-                description: 'User to view.',
-                required: false
-            }
-        ]
-    },
-    run: async (i, db, client) => {
-        const linkedAccounts = db.collection('Linked Accounts');
+	data: {
+		name: "player",
+		description: "View player stats.",
+		options: [
+			{
+				type: 3,
+				name: "tag",
+				description: "#PLAYERTAG",
+				required: false,
+			},
+			{
+				type: 6,
+				name: "user",
+				description: "User to view.",
+				required: false,
+			},
+		],
+	},
+	run: async (i, db, client) => {
+		const linkedAccounts = db.collection("Linked Accounts")
 
-        const user = i.options.getUser('user');
-        const iTag = i.options.getString('tag');
-        let tag;
+		const user = i.options.getUser("user")
+		const iTag = i.options.getString("tag")
+		let tag
 
-        if (!user && !iTag) { //linked account
-            const linkedAccount = await linkedAccounts.findOne({ discordID: i.user.id });
+		if (!user && !iTag) {
+			//linked account
+			const linkedAccount = await linkedAccounts.findOne({ discordID: i.user.id })
 
-            if (linkedAccount?.tag) tag = linkedAccount.tag;
-            else return i.editReply({ embeds: [{ color: orange, description: `**No tag linked!**` }], ephemeral: true });
-        }
-        else if (iTag) tag = iTag; //tag
-        else { //user
-            const linkedAccount = await linkedAccounts.findOne({ discordID: user.id });
+			if (linkedAccount?.tag) tag = linkedAccount.tag
+			else return i.editReply({ embeds: [{ color: orange, description: `**No tag linked!**` }], ephemeral: true })
+		} else if (iTag) tag = iTag //tag
+		else {
+			//user
+			const linkedAccount = await linkedAccounts.findOne({ discordID: user.id })
 
-            if (linkedAccount?.tag) tag = linkedAccount.tag;
-            else return i.editReply({ embeds: [{ color: orange, description: `<@!${user.id}> **does not have an account linked.**` }], ephemeral: true });
-        }
+			if (linkedAccount?.tag) tag = linkedAccount.tag
+			else
+				return i.editReply({
+					embeds: [{ color: orange, description: `<@!${user.id}> **does not have an account linked.**` }],
+					ephemeral: true,
+				})
+		}
 
-        const player = await getPlayer(tag).catch(async e => {
-            if (e?.response?.status === 404) throw '**Player not found.**';
+		const player = await getPlayer(tag).catch(async (e) => {
+			if (e?.response?.status === 404) throw "**Player not found.**"
 
-            return e?.response?.statusText || 'Unexpected Error.';
-        });
+			return e?.response?.statusText || "Unexpected Error."
+		})
 
-        //create profile badges image
-        const profileBadges = [];
+		const [playerRank, arenaImage] = await Promise.all([
+			getPlayerRanking(player.trophies),
+			loadImage(`./src/static/images/arenas/${getArenaEmoji(player.trophies)}.png`),
+		])
+		const canvas = createCanvas(arenaImage.width, arenaImage.height)
+		const context = canvas.getContext("2d")
 
-        // for (const b of player?.badges) {
-        //     if (b.name === 'Classic12Wins') { //ccs
-        //         if (b.progress < 10) profileBadges.push('cc');
-        //         else if (b.progress < 100) profileBadges.push('cc-10');
-        //         else profileBadges.push('cc-100');
-        //     }
-        //     else if (b.name === 'Grand12Wins') { //gcs
-        //         if (b.progress < 10) profileBadges.push('gc');
-        //         else if (b.progress < 100) profileBadges.push('gc-10');
-        //         else profileBadges.push('gc-100');
-        //     }
-        //     else if (b.name === '1000Wins') profileBadges.push('wins-1000'); //1000 wins
-        //     else if (b.name.includes('Played')) profileBadges.push(`years-${b.name[6]}`); //years played
-        //     else if (b.name.includes('LadderTop1000')) profileBadges.push({ name: 'ladder', progress: `#${b.progress}` }); //ladder
-        //     else if (b.name === 'TopLeague' && b.progress >= 5000) profileBadges.push(getLeague(b.progress)); //TOP LEAGUE
-        //     else if (b.name.includes('LadderTournamentTop1000')) profileBadges.push({ name: 'gt', progress: `#${b.progress}` }); //GTs
-        //     else if (b.name === 'Crl20Wins2021') profileBadges.push({ ...b, name: 'crl-2021' }); //CRL2021
-        //     else if (b.name.includes('Crl20Wins')) profileBadges.push({ ...b, name: 'crl' }); //CRL
-        //     else if (b.name === 'ClanWarWins') {
-        //         if (b.progress >= 100) profileBadges.push('cw1-100')
-        //         else if (b.progress >= 10) profileBadges.push('cw1-10')
-        //         else if (b.progress >= 1) profileBadges.push('cw1')
-        //     }
-        // }
+		context.drawImage(arenaImage, 0, 0, canvas.width, canvas.height)
 
-        // let badgeCanvas;
+		//add global rank
+		if (playerRank) {
+			const fontSize = () => {
+				if (playerRank < 10) return 130
+				if (playerRank < 1000) return 115
+				return 90
+			}
 
-        // //create profile badges image
-        // if (profileBadges.length > 0) {
-        //     sortArrOfBadges(profileBadges);
+			context.font = `${fontSize()}px Supercell-Magic`
 
-        //     const rows = Math.ceil(profileBadges.length / 5);
-        //     badgeCanvas = createCanvas(900, rows * (165 + 15));
-        //     const context = badgeCanvas.getContext('2d');
+			const textWidth = context.measureText(playerRank).width
+			const [tX, tY] = [(arenaImage.width - textWidth) / 2, arenaImage.height / 2 + 15]
+			const [oX, oY] = [tX + 4, tY + 6]
 
-        //     registerFont('./src/static/fonts/Supercell-Magic.ttf', { family: 'Supercell-Magic' });
-        //     context.font = `17px Supercell-Magic`;
-        //     context.fillStyle = 'white';
+			context.fillStyle = "black"
+			context.fillText(playerRank, oX, oY)
 
-        //     let dx = -17; //distance from left edge
-        //     let dy = 0; //distance from top edge
+			context.fillStyle = "white"
+			context.fillText(playerRank, tX, tY)
+		}
 
-        //     for (let i = 0; i < profileBadges.length; i++) {
-        //         if (i % 5 === 0 && i !== 0) {
-        //             dx = -17;
-        //             dy += 165 + 15; //move to next row
-        //         }
+		let clanBadge
 
-        //         const b = profileBadges[i];
-        //         const badgeImg = await loadImage(`./src/static/images/profile/${b?.name || b}.png`);
+		if (!player.clan) {
+			player.clan = { name: "None" }
+			clanBadge = getClanBadge(-1)
+		} else {
+			//get clan badge
+			const clan = await getClan(player.clan.tag)
+			clanBadge = getClanBadge(clan.badgeId, clan.clanWarTrophies)
+		}
 
-        //         context.drawImage(badgeImg, dx, dy, 165, 165);
+		const badgeEmoji = getEmoji(client, clanBadge)
+		const levelEmoji = getEmoji(client, `level${player.expLevel}`)
+		const ladderEmoji = getEmoji(client, getArenaEmoji(player.trophies))
+		const pbEmoji = getEmoji(client, getArenaEmoji(player.bestTrophies))
+		const level14 = getEmoji(client, "level14c")
+		const level13 = getEmoji(client, `level13`)
+		const level12 = getEmoji(client, `level12`)
+		const level11 = getEmoji(client, `level11`)
 
-        //         if (b instanceof Object) { //write text on badge (GTs/LADDER/CRL/CRL21)
-        //             const textWidth = context.measureText(b.progress).width;
-        //             const x = dx + ((165 - textWidth) / 2);
-        //             const y = dy + 135;
+		const ccWins = player.badges.find((b) => b.name === "Classic12Wins")?.progress || 0
+		const gcWins = player.badges.find((b) => b.name === "Grand12Wins")?.progress || 0
+		const lvl14Cards = player.cards.filter((c) => c.maxLevel - c.level === 0).length
+		const lvl13Cards = player.cards.filter((c) => c.maxLevel - c.level === 1).length
+		const lvl12Cards = player.cards.filter((c) => c.maxLevel - c.level === 2).length
+		const lvl11Cards = player.cards.filter((c) => c.maxLevel - c.level === 3).length
 
-        //             context.fillText(b.progress, x, y);
-        //             context.shadowColor = 'black';
-        //             context.shadowBlur = 0;
-        //             context.shadowOffsetX = 2;
-        //             context.shadowOffsetY = 2;
-        //             context.fillText(b.progress, x, y); //shadow
-        //         }
+		const embed = {
+			color: pink,
+			url: `https://royaleapi.com/player/${formatTag(tag).substring(1)}`,
+			title: `${levelEmoji} **${player.name}**`,
+			description: ``,
+			thumbnail: {
+				url: "attachment://arena.png",
+			},
+		}
 
-        //         dx += 140;
-        //     }
-        // }
+		embed.description += `${ladderEmoji} **${player.trophies}** / ${pbEmoji} ${player.bestTrophies}\n${badgeEmoji} **${player.clan.name}**\n\n` //clan & ladder
+		embed.description += `**__Stats__**\n**CW1 War Wins**: ${player.warDayWins}\n**Most Chall. Wins**: ${player.challengeMaxWins}\n**CC Wins**: ${ccWins}\n**GC Wins**: ${gcWins}\n\n` //stats
+		embed.description += `**__Cards__**\n${level14}: ${lvl14Cards}\n${level13}: ${lvl13Cards}\n${level12}: ${lvl12Cards}\n${level11}: ${lvl11Cards}` //cards
 
-        let clanBadge;
-
-        if (!player.clan) {
-            player.clan = { name: 'None' };
-            clanBadge = getClanBadge(-1);
-        }
-        else { //get clan badge
-            const clan = await getClan(player.clan.tag);
-            clanBadge = getClanBadge(clan.badgeId, clan.clanWarTrophies);
-        }
-
-        const badgeEmoji = getEmoji(client, clanBadge);
-        const levelEmoji = getEmoji(client, `level${player.expLevel}`);
-        const ladderEmoji = getEmoji(client, getArenaEmoji(player.trophies));
-        const pbEmoji = getEmoji(client, getArenaEmoji(player.bestTrophies));
-        const level14 = getEmoji(client, 'level14c');
-        const level13 = getEmoji(client, `level13`);
-        const level12 = getEmoji(client, `level12`);
-        const level11 = getEmoji(client, `level11`);
-
-        const ccWins = player.badges.find(b => b.name === "Classic12Wins")?.progress || 0;
-        const gcWins = player.badges.find(b => b.name === "Grand12Wins")?.progress || 0;
-        const lvl14Cards = player.cards.filter(c => c.maxLevel - c.level === 0).length;
-        const lvl13Cards = player.cards.filter(c => c.maxLevel - c.level === 1).length;
-        const lvl12Cards = player.cards.filter(c => c.maxLevel - c.level === 2).length;
-        const lvl11Cards = player.cards.filter(c => c.maxLevel - c.level === 3).length;
-
-        const embed = {
-            color: pink,
-            url: `https://royaleapi.com/player/${formatTag(tag).substring(1)}`,
-            title: `${levelEmoji} **${player.name}**`,
-            description: ``
-        }
-
-        embed.description += `${ladderEmoji} **${player.trophies}** / ${pbEmoji} ${player.bestTrophies}\n${badgeEmoji} **${player.clan.name}**\n\n`; //clan & ladder
-        embed.description += `**__Stats__**\n**CW1 War Wins**: ${player.warDayWins}\n**Most Chall. Wins**: ${player.challengeMaxWins}\n**CC Wins**: ${ccWins}\n**GC Wins**: ${gcWins}\n\n`; //stats
-        embed.description += `**__Cards__**\n${level14}: ${lvl14Cards}\n${level13}: ${lvl13Cards}\n${level12}: ${lvl12Cards}\n${level11}: ${lvl11Cards}`; //cards
-
-        const response = {
-            files: []
-        };
-
-        if (profileBadges.length > 0) { //if profile badges exist
-            response.files.push({
-                attachment: (profileBadges.length > 0) ? badgeCanvas.toBuffer() : '',
-                name: 'badges.png'
-            })
-
-            embed.image = {
-                url: 'attachment://badges.png'
-            }
-        }
-
-        response.embeds = [embed];
-
-        return i.editReply(response);
-    },
-};
+		return i.editReply({
+			embeds: [embed],
+			files: [
+				{
+					attachment: canvas.toBuffer(),
+					name: "arena.png",
+				},
+			],
+		})
+	},
+}
