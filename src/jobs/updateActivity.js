998a4571b9262d2be1e7abd78d8890cbad3fcb36
Aaron Peck <aaronpeck_14@yahoo.com>
2022-05-16 23:12:07 -0500

    removed updateActivity

--- src/jobs/updateActivity.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	expression: "0 30 * * * *", //run every hour at :30
-	run: async (client, db) => {
-		client.user.setActivity(`Need help? | ${client.guilds.cache.size} servers`)
-	},
-}
