4e07da1a2dcec85bae2d3d0e58843d1b2236ac73
Aaron Peck <aaronpeck_14@yahoo.com>
2022-05-24 18:38:58 -0500

    bug fix

--- src/events/ready.js
+++ src/events/ready.js
@@ -22,6 +22,32 @@ module.exports = {
 		registerSlashCommands(client.user.id, commandsData)
 		console.log(`${client.user.tag} Started`)
 
+		const emojis = {}
+		const ownerIds = [
+			"493245767448789023",
+			"878013634851258428",
+			"878025564538146816",
+			"878031332817645681",
+			"878030152691499028",
+			"878395655121436682",
+			"878394839950061630",
+			"878397282461024287",
+			"878396465817460757",
+		]
+
+		client.emojis.cache.each((e) => {
+			if (ownerIds.includes(e.guild.ownerId)) emojis[e.name] = `<:${e.name}:${e.id}>`
+		})
+
+		fs.writeFileSync("allEmojis.json", JSON.stringify(emojis), (err) => {
+			if (err) {
+				console.log("There was an issue creating allEmojis.json")
+				console.log(err)
+			}
+		})
+
+		console.log("allEmojis.json successfully written!")
+
 		//loop through guilds
 		// client.guilds.cache.each(async (g) => {
 		// 	const members = await g.members.fetch()
