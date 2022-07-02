0817446da790c1ff80282e1dccb8b1a56451cefe
Aaron Peck <aaronpeck_14@yahoo.com>
2022-07-02 17:44:38 -0500

    added CW2 wins

--- src/commands/player.js
+++ src/commands/player.js
@@ -108,6 +108,7 @@ module.exports = {
 
 		const ccWins = player.badges.find((b) => b.name === "Classic12Wins")?.progress || 0
 		const gcWins = player.badges.find((b) => b.name === "Grand12Wins")?.progress || 0
+		const cw2Wins = player.badges.find((b) => b.name === "ClanWarWins")?.progress || 0
 		const lvl14Cards = player.cards.filter((c) => c.maxLevel - c.level === 0).length
 		const lvl13Cards = player.cards.filter((c) => c.maxLevel - c.level === 1).length
 		const lvl12Cards = player.cards.filter((c) => c.maxLevel - c.level === 2).length
@@ -126,7 +127,7 @@ module.exports = {
 		embed.description += `${ladderEmoji} **${player.trophies}** / ${pbEmoji} ${player.bestTrophies}\n${badgeEmoji} **${formatStr(player.clan.name)}**${
 			player.role ? ` (${formatRole(player.role)})` : ""
 		}\n\n`
-		embed.description += `**__Stats__**\n**CW1 War Wins**: ${player.warDayWins}\n**Most Chall. Wins**: ${player.challengeMaxWins}\n**CC Wins**: ${ccWins}\n**GC Wins**: ${gcWins}\n\n` //stats
+		embed.description += `**__Stats__**\n**CW1 Wins**: ${player.warDayWins}\n**CW2 Wins**: ${cw2Wins}\n**Most Chall. Wins**: ${player.challengeMaxWins}\n**CC Wins**: ${ccWins}\n**GC Wins**: ${gcWins}\n\n` //stats
 		embed.description += `**__Cards__**\n${level14}: ${lvl14Cards}\n${level13}: ${lvl13Cards}\n${level12}: ${lvl12Cards}\n${level11}: ${lvl11Cards}` //cards
 
 		return i.editReply({
