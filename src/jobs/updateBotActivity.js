c42cd47a887acf233891008bfd15daeff751deca
Aaron Peck <aaronpeck_14@yahoo.com>
2022-07-28 15:52:55 -0500

    comment fix

--- src/jobs/updateBotActivity.js
+++ src/jobs/updateBotActivity.js
@@ -1,5 +1,5 @@
 module.exports = {
-	expression: "0 */20 * * * *", //run every 30 mins
+	expression: "0 */20 * * * *", //run every 20 mins
 	run: async (client, db) => {
 		client.user.setActivity(`Need help? | ${client.guilds.cache.size} servers`)
 	},
