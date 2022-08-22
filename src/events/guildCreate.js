f7c3b69535cf2bb1be167dc2be83a3cb0a506571
Aaron Peck <aaronpeck_14@yahoo.com>
2022-08-21 22:14:30 -0500

    added support server logging

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,3 +1,5 @@
+const { green } = require("../static/colors")
+
 module.exports = {
 	event: "guildCreate",
 	run: async (client, db, guild) => {
@@ -13,6 +15,22 @@ module.exports = {
 			abbreviations: [],
 		})
 
-		console.log(`JOINED GUILD: ${guild.name} (${guild.id})`)
+		try {
+			//send log message to support server
+			client.channels.cache.get("947608454456016896").send({
+				embeds: [
+					{
+						title: "Joined Server!",
+						description: `Name: ${guild.name}\nID: ${guild.id}\nMembers: ${guild.memberCount}`,
+						color: green,
+						thumbnail: {
+							url: `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`,
+						},
+					},
+				],
+			})
+		} catch {
+			console.log("Error sending join embed to Support Server")
+		}
 	},
 }
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,3 +1,5 @@
+const { green } = require("../static/colors")
+
 module.exports = {
 	event: "guildDelete",
 	run: async (client, db, guild) => {
@@ -7,6 +9,24 @@ module.exports = {
 			guilds.deleteOne({ guildID: guild.id })
 
 			console.log(`LEFT GUILD: ${guild.name} (${guild.id})`)
+		}
+
+		try {
+			//send log message to support server
+			client.channels.cache.get("947608454456016896").send({
+				embeds: [
+					{
+						title: "Left Server!",
+						description: `Name: ${guild.name}\nID: ${guild.id}\nMembers: ${guild.memberCount}`,
+						color: green,
+						thumbnail: {
+							url: `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`,
+						},
+					},
+				],
+			})
+		} catch {
+			console.log("Error sending leave embed to Support Server")
 		}
 	},
 }
