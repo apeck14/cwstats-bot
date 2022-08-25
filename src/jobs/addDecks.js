554c8bf0f55de66d4d05e565cb8a4262eaf0ec48
Aaron Peck <aaronpeck_14@yahoo.com>
2022-08-25 11:11:49 -0500

    bug fix

--- src/jobs/addDecks.js
+++ src/jobs/addDecks.js
@@ -9,7 +9,7 @@ puppeteer.use(StealthPlugin())
 puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
 
 module.exports = {
-	expression: "0 0 2 * * *", //run once a day
+	expression: "0 15 16 * * *", //run once a day
 	run: async (client, db) => {
 		const decks = db.collection("Decks")
 
