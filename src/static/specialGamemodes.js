736f75696f5518b860303070de8952866de7a957
Aaron Peck <aaronpeck_14@yahoo.com>
2023-04-28 21:02:06 -0500

    added /spy

---
+++ src/static/specialGamemodes.js
@@ -0,0 +1,27 @@
+module.exports = [
+  { name: "RampUpElixir_Ladder", str: "1v1 Ramp Up", emoji: "rampup" },
+  { name: "Overtime_Ladder", str: "1v1 Sudden Death", emoji: "suddendeath" },
+  {
+    name: "TripleElixir_Ladder",
+    str: "1v1 Triple Elixir",
+    emoji: "tripleelixir",
+  },
+  { name: "Touchdown", str: "1v1 Touchdown", emoji: "touchdown" },
+  {
+    name: "DoubleElixir_Ladder",
+    str: "1v1 Double Elixir",
+    emoji: "doubleelixir",
+  },
+  { name: "Rage_Ladder", str: "1v1 Rage", emoji: "rage_1v1" },
+  {
+    name: "7xElixir_Ladder",
+    str: "1v1 7x Elixir",
+    emoji: "infiniteelixir",
+  },
+  {
+    name: "ElixirCollectorCapture_Ladder",
+    str: "1v1 Elixir Capture",
+    emoji: "elixircapture",
+  },
+  { name: "Heist_Ladder", str: "1v1 Heist", emoji: "heist" },
+]
