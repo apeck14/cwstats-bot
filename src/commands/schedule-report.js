7d66047d2586e91667dd449a07c83afb6f382aa3
Aaron Peck <aaronpeck_14@yahoo.com>
2023-05-12 16:23:56 -0500

    bug fix

--- src/commands/schedule-report.js
+++ src/commands/schedule-report.js
@@ -27,28 +27,6 @@ module.exports = {
     },
     options: [
       {
-        type: 3,
-        name: "tag",
-        name_localizations: {
-          de: "kennzeichnung",
-          fr: "balise",
-          "es-ES": "etiqueta",
-          tr: "etiket",
-          it: "tag",
-          nl: "tag",
-        },
-        description: "Clan tag (#ABC123) or abbreviation",
-        description_localizations: {
-          de: "Clan-Tag (#ABC123) oder Abkürzung",
-          fr: "Tag du clan (#ABC123) ou abréviation",
-          "es-ES": "Etiqueta del clan (#ABC123) o abreviatura",
-          tr: "Klan etiketi (#ABC123) veya kısaltma",
-          it: "Tag del clan (#ABC123) o abbreviazione",
-          nl: "Clan tag (#ABC123) of afkorting",
-        },
-        required: false,
-      },
-      {
         type: 7,
         name: "channel",
         name_localizations: {
@@ -119,6 +97,28 @@ module.exports = {
         required: true,
         min_value: 0,
         max_value: 59,
+      },
+      {
+        type: 3,
+        name: "tag",
+        name_localizations: {
+          de: "kennzeichnung",
+          fr: "balise",
+          "es-ES": "etiqueta",
+          tr: "etiket",
+          it: "tag",
+          nl: "tag",
+        },
+        description: "Clan tag (#ABC123) or abbreviation",
+        description_localizations: {
+          de: "Clan-Tag (#ABC123) oder Abkürzung",
+          fr: "Tag du clan (#ABC123) ou abréviation",
+          "es-ES": "Etiqueta del clan (#ABC123) o abreviatura",
+          tr: "Klan etiketi (#ABC123) veya kısaltma",
+          it: "Tag del clan (#ABC123) o abbreviazione",
+          nl: "Clan tag (#ABC123) of afkorting",
+        },
+        required: false,
       },
     ],
   },
