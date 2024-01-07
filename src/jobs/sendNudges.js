b50254e8db670b388d816c407cbaebbfc3045e38
Aaron Peck <aaronpeck_14@yahoo.com>
2024-01-07 17:11:07 -0500

    fixed logging

--- src/jobs/sendNudges.js
+++ src/jobs/sendNudges.js
@@ -93,7 +93,7 @@ module.exports = {
               )}.`
 
           console.log(`Scheduled nudge error: ${clanTag}`)
-          console.log(clan?.error || race?.error)
+          console.log(clanError || raceError)
 
           nudgeChannel.send({ embeds: [{ color: red, description: errMsg }] })
 
