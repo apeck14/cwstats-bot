94400b04275d2ce744b6267a6500e0ba206d4579
Aaron Peck <aaronpeck_14@yahoo.com>
2024-03-11 18:36:49 -0400

    feat: added 4 new regions

--- src/static/locations.js
+++ src/static/locations.js
@@ -434,7 +434,9 @@ module.exports = [
     name: "Cyprus",
   },
   {
+    flagEmoji: ":flag_cz:",
     id: 57000070,
+    isAdded: true,
     isCountry: true,
     key: "CZ",
     name: "Czech Republic",
@@ -756,7 +758,9 @@ module.exports = [
     name: "Jamaica",
   },
   {
+    flagEmoji: ":flag_jp:",
     id: 57000122,
+    isAdded: true,
     isCountry: true,
     key: "JP",
     name: "Japan",
@@ -1150,7 +1154,9 @@ module.exports = [
     name: "Pitcairn Islands",
   },
   {
+    flagEmoji: ":flag_pl:",
     id: 57000187,
+    isAdded: true,
     isCountry: true,
     key: "PL",
     name: "Poland",
@@ -1512,7 +1518,9 @@ module.exports = [
     name: "Uganda",
   },
   {
+    flagEmoji: ":flag_ua:",
     id: 57000246,
+    isAdded: true,
     isCountry: true,
     key: "UA",
     name: "Ukraine",
