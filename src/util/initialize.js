c0f08da26880d1c061330652776c9d31e04aeb1f
Aaron Peck <aaronpeck_14@yahoo.com>
2024-10-10 14:13:14 -0700

    fix: add webhooks intent

--- src/util/initialize.js
+++ src/util/initialize.js
@@ -24,6 +24,7 @@ const initializeClient = async () => {
       GatewayIntentBits.GuildMessageTyping,
       GatewayIntentBits.GuildMessageReactions,
       GatewayIntentBits.GuildEmojisAndStickers,
+      GatewayIntentBits.GuildWebhooks,
     ],
     presence: {
       activities: [
