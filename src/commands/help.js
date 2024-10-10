3d7d669c621411566c6bda57e5ac1189610ba3e5
Aaron Peck <aaronpeck_14@yahoo.com>
2024-10-09 23:53:54 -0700

    feat: add help command

---
+++ src/commands/help.js
@@ -0,0 +1,38 @@
+const { pink } = require("../static/colors")
+
+module.exports = {
+  data: {
+    description: "Need help? Get started here.",
+    description_localizations: {
+      de: "Brauchen Sie Hilfe? Starten Sie hier.",
+      "es-ES": "¿Necesitas ayuda? Comienza aquí.",
+      fr: "Besoin d'aide ? Commencez ici.",
+      it: "Hai bisogno di aiuto? Inizia qui.",
+      nl: "Hulp nodig? Begin hier.",
+      tr: "Yardıma mı ihtiyacınız var? Buradan başlayın.",
+    },
+    name: "help",
+    name_localizations: {
+      de: "hilfe",
+      "es-ES": "ayuda",
+      fr: "aide",
+      it: "aiuto",
+      nl: "help",
+      tr: "yardım",
+    },
+  },
+  run: async (i) => {
+    const embed = {
+      color: pink,
+      description: `### [**Getting Started**](https://www.cwstats.com/docs/getting-started)\nFollow simple set up instructions to help you get started using CWStats!\n### [**Support Server**](https://discord.com/invite/fFY3cnMmnH)\nHave questions, feature requests, or bug findings? Join the Support Server.`,
+      thumbnail: {
+        url: "https://i.imgur.com/VAPR8Jq.png",
+      },
+      title: "__Need Help?__",
+    }
+
+    return i.editReply({
+      embeds: [embed],
+    })
+  },
+}
