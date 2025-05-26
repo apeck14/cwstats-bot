5d48e5cf7ef72278e500bfc803cc1d7eba42e897
Aaron Peck <aaronpeck_14@yahoo.com>
2025-05-25 19:52:21 -0700

    fix: use correct base url

--- src/util/services.js
+++ src/util/services.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 const { formatTag } = require("./formatting")
 const { INTERNAL_API_KEY } = require("../../config")
 
-const BASE_URL = "http://127.0.0.1:5000"
+const BASE_URL = "https://api.cwstats.com/api"
 
 const handleAPISuccess = (e) => e?.data
 
