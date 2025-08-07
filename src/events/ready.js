27d5183e82e15567857749102c33261dc6a3456d
Aaron Peck <aaronpeck_14@yahoo.com>
2025-08-06 22:21:12 -0700

    temp: add logging

--- src/events/ready.js
+++ src/events/ready.js
@@ -11,56 +11,60 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   run: async (client) => {
-    const emojis = []
+    try {
+      const emojis = []
 
-    client.emojis.cache.each((e) => {
-      if (ownerIds.includes(e.guild.ownerId)) {
-        const emoji = `<:${e.name}:${e.id}>`
-        client.cwEmojis.set(e.name, emoji)
+      client.emojis.cache.each((e) => {
+        if (ownerIds.includes(e.guild.ownerId)) {
+          const emoji = `<:${e.name}:${e.id}>`
+          client.cwEmojis.set(e.name, emoji)
 
-        emojis.push({ emoji, name: e.name })
+          emojis.push({ emoji, name: e.name })
+        }
+      })
+
+      bulkAddEmojis(emojis)
+
+      client.commandsWebhook = new WebhookClient({ url: COMMANDS_WEBHOOK_URL })
+      client.botWebhook = new WebhookClient({ url: BOT_WEBHOOK_URL })
+
+      const commandFiles = fs.readdirSync("./src/commands")
+      const contextCommandFiles = fs.readdirSync("./src/context-commands")
+
+      const commandsArray = []
+
+      // add slash commands
+      for (const file of commandFiles) {
+        const command = require(`../commands/${file}`)
+        client.commands.set(command.data.name, command)
+
+        commandsArray.push(command)
       }
-    })
 
-    bulkAddEmojis(emojis)
+      // add context menu commands
+      for (const file of contextCommandFiles) {
+        const command = require(`../context-commands/${file}`)
+        client.contextCommands.set(command.data.name, command)
 
-    client.commandsWebhook = new WebhookClient({ url: COMMANDS_WEBHOOK_URL })
-    client.botWebhook = new WebhookClient({ url: BOT_WEBHOOK_URL })
+        commandsArray.push(command)
+      }
 
-    const commandFiles = fs.readdirSync("./src/commands")
-    const contextCommandFiles = fs.readdirSync("./src/context-commands")
+      const commandsData = commandsArray.map((e) => e.data)
 
-    const commandsArray = []
+      registerSlashCommands(client.user.id, commandsData)
 
-    // add slash commands
-    for (const file of commandFiles) {
-      const command = require(`../commands/${file}`)
-      client.commands.set(command.data.name, command)
+      console.log(`${client.user.tag} Started`)
 
-      commandsArray.push(command)
+      logToSupportServer(
+        client,
+        {
+          color: orange,
+          title: "Bot restarted!",
+        },
+        false,
+      )
+    } catch (e) {
+      console.log("BOT FAILED ON READY", e)
     }
-
-    // add context menu commands
-    for (const file of contextCommandFiles) {
-      const command = require(`../context-commands/${file}`)
-      client.contextCommands.set(command.data.name, command)
-
-      commandsArray.push(command)
-    }
-
-    const commandsData = commandsArray.map((e) => e.data)
-
-    registerSlashCommands(client.user.id, commandsData)
-
-    console.log(`${client.user.tag} Started`)
-
-    logToSupportServer(
-      client,
-      {
-        color: orange,
-        title: "Bot restarted!",
-      },
-      false,
-    )
   },
 }
