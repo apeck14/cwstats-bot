9a2deab99017084d3754d72e4287593d26d4530a
Aaron Peck <aaronpeck_14@yahoo.com>
2025-08-15 16:37:41 -0700

    chore: logging

--- src/util/logging.js
+++ src/util/logging.js
@@ -1,12 +1,13 @@
-const logToSupportServer = (client, embed, isCommand = true) => {
+const logToSupportServer = async (client, embed, isCommand = true) => {
   const webhook = isCommand ? client.commandsWebhook : client.botWebhook
 
-  console.log({ webhook })
   if (!webhook) return
 
-  webhook.send({ embeds: [embed] }).catch((e) => {
+  const res = await webhook.send({ embeds: [embed] }).catch((e) => {
     console.error("Error sending embed to Support Server:", e)
   })
+
+  console.log({ res })
 }
 
 module.exports = {
