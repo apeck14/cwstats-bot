1c0631fb56d965eed75b706fed029318c84a36d6
Aaron Peck <aaronpeck_14@yahoo.com>
2025-08-15 00:39:39 -0700

    refactor: add timeout to all requests

--- src/util/services.js
+++ src/util/services.js
@@ -4,255 +4,123 @@ const { INTERNAL_API_KEY } = require("../../config")
 
 const BASE_URL = "https://api.cwstats.com"
 
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { Authorization: `Bearer ${INTERNAL_API_KEY}` },
+  timeout: 15000,
+})
+
 const handleAPISuccess = (e) => e?.data
 
 // Format error messages to make them more user friendly
 const handleAPIFailure = (e, notFoundMessage = `**Not found.**`) => {
-  const { response, status } = e || {}
-  const { data } = response || {}
+  const { code, response } = e || {}
+  const { data, status } = response || {}
 
   let error = `**Unexpected error.** Please try again.`
 
-  if (status === 404) error = notFoundMessage
+  if (code === "ECONNABORTED") error = "**Request timed out.** Please try again later."
+  else if (status === 404) error = notFoundMessage
   else if (status === 429) error = `**Rate limit exceeded.** Please try again later.`
   else if (status === 503) error = `:tools: **Maintenence break.**`
-  else if (status !== 500 && data?.error) error = `**${data.error}**`
+  else if (status && status !== 500 && data?.error) error = `**${data.error}**`
 
   return { error, status }
 }
 
 const getGuild = (id, limited = false) =>
-  axios
-    .get(`${BASE_URL}/guild/${id}${limited ? "/limited" : ""}`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/guild/${id}${limited ? "/limited" : ""}`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Guild not found.**"))
 
 const getGuildLinkedClans = (id) =>
-  axios
-    .get(`${BASE_URL}/guild/${id}/clans`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/guild/${id}/clans`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Guild not found.**"))
 
 const getPlayer = (tag, limited = false) =>
-  axios
-    .get(`${BASE_URL}/player/${formatTag(tag, false)}${limited ? "/limited" : ""}`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/player/${formatTag(tag, false)}${limited ? "/limited" : ""}`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Player not found.**"))
 
 const getPlayerScores = (tag) =>
-  axios
-    .get(`${BASE_URL}/player/${formatTag(tag, false)}/scores`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/player/${formatTag(tag, false)}/scores`)
     .then(handleAPISuccess)
     .catch(handleAPIFailure)
 
 const addPlayer = (tag) =>
-  axios
-    .put(
-      `${BASE_URL}/player`,
-      {
-        tag: formatTag(tag, false),
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${INTERNAL_API_KEY}`,
-        },
-      },
-    )
+  api
+    .put("/player", { tag: formatTag(tag, false) })
     .then(handleAPISuccess)
     .catch(handleAPIFailure)
 
 const linkPlayer = (tag, userId) =>
-  axios
-    .put(
-      `${BASE_URL}/player/link`,
-      { tag, userId },
-      {
-        headers: {
-          Authorization: `Bearer ${INTERNAL_API_KEY}`,
-        },
-      },
-    )
-    .then(handleAPISuccess)
-    .catch(handleAPIFailure)
+  api.put("/player/link", { tag, userId }).then(handleAPISuccess).catch(handleAPIFailure)
 
 const getClan = (tag, limited = false) =>
-  axios
-    .get(`${BASE_URL}/clan/${formatTag(tag, false)}${limited ? "/limited" : ""}`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/clan/${formatTag(tag, false)}${limited ? "/limited" : ""}`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Clan not found.**"))
 
 const getRace = (tag, limited = false) =>
-  axios
-    .get(`${BASE_URL}/clan/${formatTag(tag, false)}/race${limited ? "/limited" : ""}`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/clan/${formatTag(tag, false)}/race${limited ? "/limited" : ""}`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Race not found.**"))
 
 const getDailyLeaderboard = ({ key, limit, maxTrophies, minTrophies }) =>
-  axios
-    .get(`${BASE_URL}/leaderboard/daily`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-      params: {
-        key,
-        limit,
-        maxTrophies,
-        minTrophies,
-      },
-    })
+  api
+    .get("/leaderboard/daily", { params: { key, limit, maxTrophies, minTrophies } })
     .then(handleAPISuccess)
-    .catch((e) => handleAPIFailure(e))
+    .catch(handleAPIFailure)
 
 const getAllPlusClans = (tagsOnly = false) =>
-  axios
-    .get(`${BASE_URL}/plus/clans`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-      params: {
-        tagsOnly,
-      },
-    })
-    .then(handleAPISuccess)
-    .catch((e) => handleAPIFailure(e))
+  api.get("/plus/clans", { params: { tagsOnly } }).then(handleAPISuccess).catch(handleAPIFailure)
 
 const setCommandCooldown = (id, commandName, delay) =>
-  axios
-    .patch(
-      `${BASE_URL}/guild/${id}/command-cooldown`,
-      {
-        commandName,
-        delay,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${INTERNAL_API_KEY}`,
-        },
-      },
-    )
-    .then(handleAPISuccess)
-    .catch((e) => handleAPIFailure(e))
+  api.patch(`/guild/${id}/command-cooldown`, { commandName, delay }).then(handleAPISuccess).catch(handleAPIFailure)
 
 const getLinkedAccount = (userId) =>
-  axios
-    .get(`${BASE_URL}/user/${userId}/linked-account`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/user/${userId}/linked-account`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Linked account not found.**"))
 
 const getPlayerBattleLog = (tag) =>
-  axios
-    .get(`${BASE_URL}/player/${formatTag(tag, false)}/log`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .get(`/player/${formatTag(tag, false)}/log`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Player not found.**"))
 
 const addNudgeLink = (id, tag, userId) =>
-  axios
-    .put(
-      `${BASE_URL}/guild/${id}/nudge-link`,
-      {
-        tag: formatTag(tag, false),
-        userId,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${INTERNAL_API_KEY}`,
-        },
-      },
-    )
+  api
+    .put(`/guild/${id}/nudge-link`, { tag: formatTag(tag, false), userId })
     .then(handleAPISuccess)
     .catch(handleAPIFailure)
 
 const deleteNudgeLink = (id, tag) =>
-  axios
-    .delete(`${BASE_URL}/guild/${id}/nudge-link/${formatTag(tag, false)}`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .delete(`/guild/${id}/nudge-link/${formatTag(tag, false)}`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Tag is not linked.**"))
 
-const createGuild = (id) =>
-  axios
-    .post(`${BASE_URL}/guild/${id}`, null, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
-    .then(handleAPISuccess)
-    .catch(handleAPIFailure)
+const createGuild = (id) => api.post(`/guild/${id}`).then(handleAPISuccess).catch(handleAPIFailure)
 
 const deleteGuild = (id) =>
-  axios
-    .delete(`${BASE_URL}/guild/${id}`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-    })
+  api
+    .delete(`/guild/${id}`)
     .then(handleAPISuccess)
     .catch((e) => handleAPIFailure(e, "**Guild not found.**"))
 
-const bulkAddEmojis = (emojis) =>
-  axios
-    .post(
-      `${BASE_URL}/emoji/bulk-add`,
-      {
-        emojis,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${INTERNAL_API_KEY}`,
-        },
-      },
-    )
-    .then(handleAPISuccess)
-    .catch(handleAPIFailure)
+const bulkAddEmojis = (emojis) => api.post("/emoji/bulk-add", { emojis }).then(handleAPISuccess).catch(handleAPIFailure)
 
 const getPlayerSearch = (name, limit = 10) =>
-  axios
-    .get(`${BASE_URL}/player/search`, {
-      headers: {
-        Authorization: `Bearer ${INTERNAL_API_KEY}`,
-      },
-      params: {
-        limit,
-        name,
-      },
-    })
-    .then(handleAPISuccess)
-    .catch(handleAPIFailure)
+  api.get("/player/search", { params: { limit, name } }).then(handleAPISuccess).catch(handleAPIFailure)
 
 module.exports = {
   addNudgeLink,
