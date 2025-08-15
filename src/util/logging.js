6ddd2dddcb2d4e6da9ad5032db14c863083f1b79
Aaron Peck <aaronpeck_14@yahoo.com>
2025-08-15 16:35:01 -0700

    chore: add logging

--- src/util/logging.js
+++ src/util/logging.js
@@ -1,5 +1,7 @@
 const logToSupportServer = (client, embed, isCommand = true) => {
   const webhook = isCommand ? client.commandsWebhook : client.botWebhook
+
+  console.log({ webhook })
   if (!webhook) return
 
   webhook.send({ embeds: [embed] }).catch((e) => {
