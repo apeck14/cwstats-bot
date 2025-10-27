7d3f5c7dd05dfe8ef345af107a74c6ba9b2dac23
Aaron Peck <aaronpeck_14@yahoo.com>
2025-10-26 17:04:18 -0700

    fix: add catch

--- src/util/logging.js
+++ src/util/logging.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
 const logToSupportServer = async (client, embed, isCommand = true) => {
-  // ignore dev environment
-  const webhook = isCommand ? client.commandsWebhook : client.botWebhook
+  try {
+    // ignore dev environment
+    const webhook = isCommand ? client.commandsWebhook : client.botWebhook
 
-  if (!webhook) return
+    if (!webhook) return
 
-  webhook.send({ embeds: [embed] }).catch((e) => {
+    await webhook.send({ embeds: [embed] })
+  } catch (e) {
     console.error('Error sending embed to Support Server:', e)
-  })
+  }
 }
 
 module.exports = {
